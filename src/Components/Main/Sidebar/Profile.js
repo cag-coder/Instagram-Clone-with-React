ab336be3fb6965d6cb0b882a91a6e0e17c3efd07
cagatayem <cagatayemm@gmail.com>
2021-06-03 13:28:35 +0300

    add more sidebar

--- src/Components/Main/Sidebar/Profile.js
+++ src/Components/Main/Sidebar/Profile.js
@@ -4,10 +4,12 @@ import "./Profile.css"
 function Profile({size,name,username,number,border}) {
     return (
         <div className="profile">
-            <Avatar size={size} number={number} border= {border}/>
-            <div className="profile__userInfo">
-                <h4>{name}</h4>
-                <h5>{username}</h5>
+            <div className="profile__user">
+                <Avatar size={size} number={number} border= {border}/>
+                <div className="profile__userInfo">
+                    <h4>{name}</h4>
+                    <h5>{username}</h5>
+                </div>
             </div>
             <a className="profile__link" href="/">Switch</a>
 
